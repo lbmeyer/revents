4eeb4066c26d28642e2c82e02dbd50718272c953
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2018-06-27 12:05:37 -0400

    End of Section 6: Added Routing to app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./app/layout/App";
@@ -15,14 +16,19 @@ import registerServiceWorker from "./registerServiceWorker";
 const rootEl = document.getElementById("root");
 
 let render = () => {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
 };
 
-if (module.hot) {
-  module.hot.accept("./app/layout/App", () => {
-    setTimeout(render);
-  });
-}
+// if (module.hot) {
+//   module.hot.accept("./app/layout/App", () => {
+//     setTimeout(render);
+//   });
+// }
 
 render();
 
